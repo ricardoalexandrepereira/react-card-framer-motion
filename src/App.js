c006529a3d89ef876fd79d54242b0d8ffb7f149f
ricardoalexandrepereira <ricardo_certe@hotmail.com>
2022-05-23 20:28:05 +0100

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div
+        transition={{ layout: { duration: 1, type: "spring" } }}
+        layout
+        onClick={() => setIsOpen(!isOpen)}
+        className="card"
+        style={{
+          borderRadius: "1rem",
+          boxShadow: "0px 10px 30px rgba(0,0,0,0.5)",
+          cursor: "pointer",
+        }}
+      >
+        <motion.h2 layout="position">framer motion 🚀</motion.h2>
+
+        
+        {isOpen ? (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{duration: 1}}
+            className="expand"
+          >
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Temporibus a eligendi numquam, eaque repellat in tempore eos dicta
+              mollitia voluptatibus.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptates, perspiciatis!
+            </p>
+           
+          </motion.div>
+        ) : (
+          ""
+        )}
+        
+
+      </motion.div>
     </div>
   );
 }
